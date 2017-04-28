60bb2efcfcbdd1d5bbe6f2dfb340d198a35e7818
Christoph Molnar <christoph.molnar@gmail.com>
2017-04-28 14:53:30 +0200

    fixes (hopefully) CI deployment

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-set -e
-
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
 git config --global user.email "christoph.molnar@gmail.com"
 git config --global user.name "Christoph Molnar"
 
