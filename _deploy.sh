a46d4652bef74813ac8358ac41b2d46a5a29219c
Christoph Molnar <christoph.molnar@gmail.com>
2017-04-28 15:12:09 +0200

    next try with Travis CI

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,42 @@
 #!/bin/sh
 
-git config --global user.email "christoph.molnar@gmail.com"
-git config --global user.name "Christoph Molnar"
 
-git clone  -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git xai-book
-cd xai-book
+# Copied from here: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="master"
+
+function doCompile {
+  ./_build.sh
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+# Run our compile script
+doCompile
+
+git config user.email "christoph.molnar@gmail.com"
+git config user.name "Christoph Molnar"
+
 git add --all *
-git commit -m"Update book" || true
-git push -q origin master
+git commit -m "Update book: ${SHA}"# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
