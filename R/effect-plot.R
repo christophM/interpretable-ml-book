9d11b96b6192fbbeb602da741f282ac4d147c369
Christoph Molnar <christoph.molnar@gmail.com>
2018-01-06 11:58:08 +0100

    moves functions from .Rmd files to R folder

---
+++ R/effect-plot.R
@@ -0,0 +1,41 @@
+
+#' Plot effects of linear model
+effect_plot = function(mod, dat,  feature_names=NULL){
+  X = get_effects(mod, dat)
+  if(!missing(feature_names)){
+    rownames(X) = feature_names
+  }
+  X = gather(X)
+  ggplot(X) +
+    geom_hline(yintercept=0, linetype=4) +
+    geom_boxplot(aes(x=key, y=value, group=key)) +
+    coord_flip() +
+    scale_y_continuous('Feature effect') +
+    scale_x_discrete('Feature') +
+    my_theme()
+}
+
+get_reference_dataset = function(dat){
+  df = lapply(dat, function(feature){
+    if(class(feature) == 'factor'){
+      factor(levels(feature)[1], levels = levels(feature))
+    } else {
+      0
+    }
+  })
+  data.frame(df)
+}
+
+get_effects = function(mod, dat){
+
+  X = data.frame(predict(mod, type = 'terms'))
+
+  # Nicer colnames
+  colnames(X) = gsub('^X\\.', '', colnames(X))
+  colnames(X) = gsub('\\.', ' ', colnames(X))
+
+  # predict with type='terms' centers the results, so we have to add the mean again
+  reference_X = predict(mod, newdata=get_reference_dataset(dat), type='terms')
+  X_star = data.frame(t(apply(X, 1, function(x){ x - reference_X[1,names(X)]})))
+  X_star
+}
---
+++ R/utils.R
@@ -0,0 +1,7 @@
+#' Make character vector pretty
+pretty_rownames = function(rnames){
+  rnames = gsub('^`', '', rnames)
+  rnames = gsub('`$', '', rnames)
+  rnames = gsub('`', ':', rnames)
+  rnames
+}
