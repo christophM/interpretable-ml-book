71cdc2b7ece36f281508205b779d9b2db0921d36
Christoph Molnar <christoph.molnar@gmail.com>
2018-03-08 17:03:02 +0100

    moves knitr options to R/

--- R/knitr_options.R
+++ R/knitr_options.R
@@ -1,2 +1,22 @@
 library("knitr")
-knitr::opts_chunk$set( fig.path = "images/")
+library('tm')
+library('rpart')
+library('mlr')
+library('dplyr')
+library('ggplot2')
+library('tidyr')
+library('partykit')
+library('memoise')
+library('pre')
+library('iml')
+
+opts_chunk$set(
+  echo=FALSE,
+  message=FALSE,
+  warning=FALSE,
+  out.width='80%',
+  fig.align='center', 
+  fig.path = "images/"
+)
+
+output <- opts_knit$get("rmarkdown.pandoc.to")
