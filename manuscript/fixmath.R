12193248d5830029186fefc0b7200001edab7178
Christoph Molnar <christoph.molnar@gmail.com>
2018-03-10 18:29:14 +0100

    fixes fixmath.R

--- manuscript/fixmath.R
+++ manuscript/fixmath.R
@@ -7,6 +7,7 @@ fixmath <- function(infile) {
   doc0 <- readLines(infile)
   doc <- sub("^\\\\\\[$", "{\\$\\$}", doc0, perl = TRUE)
   doc <- sub("^\\\\\\]$", "{\\/\\$\\$}", doc, perl = TRUE)
+  doc <- gsub("\\$\\$(\\S+)\\$\\$", "\\$\\1\\$", doc, perl = TRUE)
   doc <- gsub("\\$(\\S+)\\$", "{\\$\\$}\\1{\\/\\$\\$}", doc, perl = TRUE)
   writeLines(doc, infile)
 }
