eae4b921dd0c9fa0057f77326fbca72296280f2a
Christoph Molnar <christoph.molnar@gmail.com>
2022-07-12 12:42:57 +0200

    only build pdf

--- _build.sh
+++ _build.sh
@@ -8,9 +8,9 @@ cd manuscript
 # Create references
 make -B 11-references.Rmd
 # Compile pdf version of book for leanpub
-#make pdf
+make pdf
 # Compile web version of book
-make html
+# make html
 # Compile epub version of book for leanpub
 # make epub
 
