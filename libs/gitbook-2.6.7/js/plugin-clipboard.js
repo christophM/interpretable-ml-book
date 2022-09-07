2501fc567599c5c69c54a1d4677ec9c36bd4e0be
Github Action <action@github.com>
2022-09-07 12:52:15 +0000

    Update Book

--- libs/gitbook-2.6.7/js/plugin-clipboard.js
+++ libs/gitbook-2.6.7/js/plugin-clipboard.js
@@ -9,7 +9,9 @@ gitbook.require(["gitbook", "jQuery"], function(gitbook, $) {
 
     // the page.change event is thrown twice: before and after the page changes
     if (clipboard) {
-      // clipboard is already defined
+      // clipboard is already defined but we are on the same page
+      if (clipboard._prevPage === window.location.pathname) return;
+      // clipboard is already defined and url path change
       // we can deduct that we are before page changes
       clipboard.destroy(); // destroy the previous events listeners
       clipboard = undefined; // reset the clipboard object
@@ -23,6 +25,8 @@ gitbook.require(["gitbook", "jQuery"], function(gitbook, $) {
         return trigger.parentNode.textContent;
       }
     });
+
+    clipboard._prevPage = window.location.pathname
 
   });
 
