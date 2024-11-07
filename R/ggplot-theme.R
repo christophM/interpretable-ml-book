b53eb12c3a39a43613483da29a51d4ad35460ea4
Christoph Molnar <christoph.molnar@gmail.com>
2024-11-07 14:11:32 +0100

    port to Quarto

--- R/ggplot-theme.R
+++ R/ggplot-theme.R
@@ -1,14 +1,2 @@
 # load libraries
-library("ggplot2")
-library("viridis")
 
-# define graphics theme
-my_theme = function(legend.position='right'){
-  theme_bw() %+replace%
-    theme(legend.position=legend.position)
-}
-
-theme_set(my_theme())
-
-
-default_color = "azure4"
--- R/utils.R
+++ R/utils.R
@@ -11,6 +11,4 @@ year_diff = function(date1, date2){
   day_diff(date1, date2) / 365.25
 }
 
-day_diff = function(date1, date2){
-  as.numeric(difftime(as.Date(date1), as.Date(date2), units = 'days'))
-}
+
